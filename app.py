d6031f3de065e1b9cbcfd7da81d9b9bb16eac28b
FranklinMHS01 <116238974+FranklinMHS01@users.noreply.github.com>
2023-06-14 18:04:20 +0700

    app.py

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def homework_get():
     return jsonify({'messages': message_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=500, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
